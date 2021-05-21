ca061c050dc405cdbd53eda48c379b105788cad3
Ducktrasher <arielvieyra99@gmail.com>
2021-05-21 08:26:18 -0500

    Añadí algunas notas para que sea un poco mas claro el contenido Esta aprueba de algunos errores, pero no todos recomiendo realizar algunas pruebas y modifiacrlo a tus necesidades Tambien las intrucciones no son claras en si se debe guardar cuanto de cada pan asi que eso lo omiti

--- Panaderia Jazmin/Source.cpp
+++ Panaderia Jazmin/Source.cpp
@@ -14,6 +14,7 @@ int main() {
 	cout << "(N)o\t		Abandonar y ver resulatados" << endl;
 	cin >> sentinel; cout << "\n";
 	do {
+		//si desde el pricipio se dice no, romper
 		if (sentinel == 'n')
 			break;
 		cout << "Bienvenidos" << endl;
@@ -67,8 +68,15 @@ int main() {
 			cout << "4-\tConcha" << endl;
 			cout << "5-\tQuesadilla" << endl;
 			cout << "6-\tSalir" << endl;
+			//mientras see lee algun dato para el tipo de pan y no eliga opcion 6
+			// seguir preguntando si quiere algun pan
+			// si la opcion no es valida, no se cuenta ningun pan
 		} while (cin >> tipoPan && tipoPan !=6);
+
 		fseek(stdin, 0, SEEK_END);
+		//pushback hace que cada cliente tenga un elemento en el vector con el valor con el que salio del menu
+		//por ejemplo si primerCliente hace 3 opciones, entonces compraPorCliente[0] = 3
+		//segunda persona hace 8 opciones, entonces compraPorCliente[1] = 8 y asi...
 		compraPorCliente.push_back(contadorPan);
 		//se guarda la cantidad que cada quien compro en su respectivo elemento en el vector
 		cout << nombreTemp << "coompro:\t" << compraPorCliente[contadorCliente - 1] << "panes" << endl;
@@ -80,6 +88,8 @@ int main() {
 		cin >> sentinel; cout << "\n";
 		system("CLS");
 	} while (sentinel == 'S' || sentinel == 's');
+	//(N)o (no mas clientes) sale del sentinela
+	//si hubo zero clientes solo omite lo de abajo, si no, trata de llamar elementos del vecotr que no existen
 	if (contadorCliente == 0) {
 		cout << "Hasta luego gracias por visitar" << endl;
 	}
